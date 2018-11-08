4dfcd6bed39e0e547685db4e2fc901ef67434de3
Terry Moore II <terry.moore.ii@gmail.com>
2018-11-07 19:57:12 -0500

    Fix class and add script

--- src/components/Zondicon.vue
+++ src/components/Zondicon.vue
@@ -1,6 +1,6 @@
 <template>
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" :class="$attr.class">
-    <component :is="$attr.icon" />
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" :class="$attrs.class">
+    <component :is="$attrs.icon" />
   </svg>
 </template>
 
